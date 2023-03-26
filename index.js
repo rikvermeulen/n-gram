b6f4ecd36f3f410a26fff1902052985fc4029f9c
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2023-03-26 20:23:38 +0200

    fix: removed console

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const ngram = require("./src/ngram");
   const randomWord = await ngram.randomNextWord("the", bigram);
 
   // Test the functions
-  console.log("Bigrams:", bigram);
   console.log("Most likely next word for 'the':", likelyWord);
   console.log("Random next word for 'the':", randomWord);
   console.log("Generated sentence starting with 'the':", sentence);
