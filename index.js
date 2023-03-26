3c297c0e4ac58513a7e2f91eb6ac2d5ab6e05684
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2023-03-26 20:24:27 +0200

    fix: orderd func call

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const ngram = require("./src/ngram");
 (async () => {
   const bigram = await bigrams.generate();
 
-  const sentence = await ngram.generateSentence("the", 10, bigram);
-  const likelyWord = await ngram.mostLikelyNextWord("the", bigram);
-  const randomWord = await ngram.randomNextWord("the", bigram);
-
   // Test the functions
+  const likelyWord = await ngram.mostLikelyNextWord("the", bigram);
   console.log("Most likely next word for 'the':", likelyWord);
+
+  const randomWord = await ngram.randomNextWord("the", bigram);
   console.log("Random next word for 'the':", randomWord);
+
+  const sentence = await ngram.generateSentence("the", 10, bigram);
   console.log("Generated sentence starting with 'the':", sentence);
 })();
